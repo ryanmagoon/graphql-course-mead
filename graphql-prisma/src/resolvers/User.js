beff642641f718621dee1f49b3e57a508251d8ba
Ryan Magoon <mr.magoon5@gmail.com>
2019-05-08 17:16:23 -0400

    setup post/comment resolvers for prisma

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,8 +1,16 @@
 const User = {
-  posts: (parent, args, { db: { posts } }, info) =>
-    posts.filter(post => post.author === parent.id),
-  comments: (parent, args, { db: { comments } }, info) =>
-    comments.filter(comment => comment.author === parent.id)
+  posts: (parent, args, { prisma }, info) =>
+    prisma
+      .user({
+        id: parent.id
+      })
+      .posts(),
+  comments: (parent, args, { prisma }, info) =>
+    prisma
+      .user({
+        id: parent.id
+      })
+      .comments()
 }
 
 export default User
