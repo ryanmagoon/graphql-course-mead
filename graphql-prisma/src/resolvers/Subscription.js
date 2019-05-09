1e1f91bde74f44eecd66614551500324e6db4460
Ryan Magoon <mr.magoon5@gmail.com>
2019-05-09 15:25:54 -0400

    implement post subscription

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -3,21 +3,26 @@ import { prisma } from '../generated/prisma'
 const Subscription = {
   comment: {
     subscribe: async (parent, { postId }, { prisma }, info) =>
-      postId
-        ? prisma.$subscribe.comment({
-            node: {
-              post: { id: postId }
-            }
-          })
-        : prisma.$subscribe.comment(),
+      prisma.$subscribe.comment({
+        node: {
+          post: { id: postId }
+        }
+      }),
     resolve: payload => {
       console.log({ payload })
       return payload
     }
   },
   post: {
-    subscribe: (parent, args, { pubsub }, info) => {
-      return pubsub.asyncIterator('post')
+    subscribe: (parent, { published }, { pubsub }, info) =>
+      prisma.$subscribe.post({
+        node: {
+          published
+        }
+      }),
+    resolve: payload => {
+      console.log({ payload })
+      return payload
     }
   }
 }
