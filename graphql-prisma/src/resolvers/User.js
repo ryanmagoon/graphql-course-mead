c22130fbda73d39d8a385b587e74a905f12a52c7
Ryan Magoon <mr.magoon5@gmail.com>
2019-05-10 21:49:20 -0400

    implement fragment to grab emails w/o id provided

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,4 +1,14 @@
+import getUserId from '../utils/getUserId'
+
 const User = {
+  email: {
+    fragment: 'fragment userId on User { id }',
+    resolve: ({ email, id }, args, { prisma, request }, info) => {
+      const userId = getUserId(request, false)
+
+      return id === userId ? email : null
+    }
+  },
   posts: (parent, args, { prisma }, info) =>
     prisma
       .user({
