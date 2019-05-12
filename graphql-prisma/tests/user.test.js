8a707dc3d75d048c8c2c4f1a82c199ad358ea163
Ryan Magoon <mr.magoon5@gmail.com>
2019-05-12 18:18:53 -0400

    set up dummy posts

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -1,5 +1,6 @@
 import 'cross-fetch/polyfill'
 import ApolloClient, { gql } from 'apollo-boost'
+import bcrypt from 'bcryptjs'
 
 import { prisma } from '../src/generated/prisma'
 
@@ -8,7 +9,33 @@ const client = new ApolloClient({
 })
 
 beforeEach(async () => {
+  await prisma.deleteManyPosts()
   await prisma.deleteManyUsers()
+  const myUser = await prisma.createUser({
+    name: 'Jen',
+    email: 'jen@example.com',
+    password: bcrypt.hashSync('buttnugget')
+  })
+  await prisma.createPost({
+    title: 'my first post!',
+    body: 'look at my first post',
+    author: {
+      connect: {
+        id: myUser.id
+      }
+    },
+    published: false
+  })
+  await prisma.createPost({
+    title: 'my second post!',
+    body: 'look at my second post',
+    author: {
+      connect: {
+        id: myUser.id
+      }
+    },
+    published: true
+  })
 })
 
 test('should create a new user', async () => {
