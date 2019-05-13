4e73e4f8517e9998af8ae96146787bcbf58eed6d
Ryan Magoon <mr.magoon5@gmail.com>
2019-05-12 20:50:29 -0400

    implement myPosts test

--- graphql-prisma/tests/post.test.js
+++ graphql-prisma/tests/post.test.js
@@ -2,7 +2,7 @@ import 'cross-fetch/polyfill'
 import { gql } from 'apollo-boost'
 
 import { prisma } from '../src/generated/prisma'
-import seedDatabase from './utils/seedDatabase'
+import seedDatabase, { userOne } from './utils/seedDatabase'
 import getClient from './utils/getClient'
 
 const client = getClient()
@@ -21,4 +21,21 @@ test('should return only published posts from public posts query', async () => {
   const response = await client.query({ query: getPosts })
   expect(response.data.posts.length).toBe(1)
   expect(response.data.posts[0].published).toBe(true)
+})
+
+test('should return all users posts for authenticated user', async () => {
+  const authenticatedClient = getClient({ jwt: userOne.jwt })
+  const getMyPosts = gql`
+    query {
+      myPosts {
+        id
+        title
+        body
+        published
+      }
+    }
+  `
+  const { data } = await authenticatedClient.query({ query: getMyPosts })
+
+  expect(data.myPosts.length).toBe(2)
 })
